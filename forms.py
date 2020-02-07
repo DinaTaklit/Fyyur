bb37cc822d62f614fdcc2b5bafcec05d21d247a4
DinaTaklit <ez_taklit@esi.dz>
2020-02-06 23:44:48 -0800

    feat: Add some Venue and Show functions     - Fix venues with number of upcoming shows     - List Shows Items     - Fix Parser issue     - fix  'NoneType' object has no attribute 'days' in date format function of babel

--- forms.py
+++ forms.py
@@ -184,9 +184,9 @@ class ArtistForm(Form):
         # TODO implement validation logic for state
         'phone'
     )
-    image_link = StringField(
-        'image_link'
-    )
+    # image_link = StringField(
+    #     'image_link'
+    # )
     genres = SelectMultipleField(
         # TODO implement enum restriction
         'genres', validators=[DataRequired()],
